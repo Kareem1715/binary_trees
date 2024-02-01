f93b2104f38b067e2dbc7c1df15cfe7d310f817f
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-01 22:13:16 +0200

    Task 13 - Count number of nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,15 +11,8 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	binary_tree_nodes(tree->left);
-	binary_tree_nodes(tree->right);
-
-	if (!tree->parent && tree->left && tree->right)
-		return (3);
-	if ((tree->left && !tree->right) || (!tree->left && tree->right))
-		return (1);
-	return (0);
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
