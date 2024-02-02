f299d0f457f71ae279fedd4d103124ea5874e261
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-02 03:11:01 +0200

    Task 18 - Uncle of node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,15 +9,12 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent || !node->parent)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);/* Node has no uncle*/
 
-	if (node->parent->parent)
-	{
-		if (node->parent->parent->right == node->parent)
-			return (node->parent->parent->left);
-		else
-			return (node->parent->parent->right);
-	}
-	return (NULL);
+
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
 }
